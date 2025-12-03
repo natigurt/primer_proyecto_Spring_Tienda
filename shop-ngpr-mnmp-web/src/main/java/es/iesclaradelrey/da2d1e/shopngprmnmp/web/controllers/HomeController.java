1b33000fae65ccfad95391f772db98ab9bcb492b
nataliapr <nataliagalileapalmaramos@gmail.com>
2025-12-03 20:51:54 +0100

    "Primeros pasos para crear la base del proyecto."

--- shop-ngpr-mnmp-web/src/main/java/es/iesclaradelrey/da2d1e/shopngprmnmp/web/controllers/HomeController.java
+++ shop-ngpr-mnmp-web/src/main/java/es/iesclaradelrey/da2d1e/shopngprmnmp/web/controllers/HomeController.java
@@ -1,7 +1,13 @@
 package es.iesclaradelrey.da2d1e.shopngprmnmp.web.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
 
-
-
+    @GetMapping("/")
+    public String index() {
+        return "index"; //que devuelva la mv de "index.html"
+    }
 }
