b89f9d3fe1f1b16f321d177f5fd93dfdcb91f9e2
nataliapr <nataliagalileapalmaramos@gmail.com>
2025-12-03 20:37:29 +0100

    "Primeros pasos para crear la base del proyecto."

---
+++ shop-ngpr-mnmp-web/src/main/java/es/iesclaradelrey/da2d1e/shopngprmnmp/web/ShopNgprMnmpWebApplication.java
@@ -0,0 +1,13 @@
+package es.iesclaradelrey.da2d1e.shopngprmnmp.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopNgprMnmpWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopNgprMnmpWebApplication.class, args);
+    }
+
+}
---
+++ shop-ngpr-mnmp-web/src/test/java/es/iesclaradelrey/da2d1e/shopngprmnmp/web/ShopNgprMnmpWebApplicationTests.java
@@ -0,0 +1,13 @@
+package es.iesclaradelrey.da2d1e.shopngprmnmp.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopNgprMnmpWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
