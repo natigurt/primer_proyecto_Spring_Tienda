3459403d1022f223078cf337505d65a109c0b9a2
nataliapr <nataliagalileapalmaramos@gmail.com>
2025-12-05 20:56:40 +0100

    "Pagina del proyecto"

--- shop-ngpr-mnmp-web/src/main/java/es/iesclaradelrey/da2d1e/shopngprmnmp/web/controllers/HomeController.java
+++ shop-ngpr-mnmp-web/src/main/java/es/iesclaradelrey/da2d1e/shopngprmnmp/web/controllers/HomeController.java
@@ -2,13 +2,15 @@ package es.iesclaradelrey.da2d1e.shopngprmnmp.web.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String index() {
-        return "index"; //que devuelva la mv de "index.html"
+    public ModelAndView index() {
+        ModelAndView mv = new ModelAndView("index");
+        return mv; //que devuelva la mv de "index.html"
     }
 
 
